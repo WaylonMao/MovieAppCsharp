85eb268c5fb84e8f6e96000a8b33ae93ccae312b
WaylonMao <tomcat_mao@hotmail.com>
2023-07-28 14:02:41 -0600

    Create add movie page

---
+++ MovieAppCsharp/Pages/AddMovie.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MovieAppCsharp.Data.Models;
+
+namespace MoviesApp.Pages
+{
+    public class AddMovieModel : PageModel
+    {
+        // [BindProperty]
+        // public string Title { get; set; }
+        //
+        // [BindProperty]
+        // public int Rate { get; set; }
+        //
+        // [BindProperty]
+        // public string Description { get; set; }
+        
+        [BindProperty]
+        public Movie Movie { get; set; }
+        
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            string value = $"{Movie.Title}-{Movie.Rate}-{Movie.Description}";
+            return Redirect(value);
+        }
+    }
+}
