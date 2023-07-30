764d7f5ad31c180749a61b66a1e21629aa305a80
WaylonMao <tomcat_mao@hotmail.com>
2023-07-30 13:42:23 -0600

    Add movie to SQL DB

--- MovieAppCsharp/Pages/AddMovie.cshtml.cs
+++ MovieAppCsharp/Pages/AddMovie.cshtml.cs
@@ -1,35 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MovieAppCsharp.Data;
 using MovieAppCsharp.Data.Models;
 
 namespace MoviesApp.Pages
 {
     public class AddMovieModel : PageModel
     {
-        // [BindProperty]
-        // public string Title { get; set; }
-        //
-        // [BindProperty]
-        // public int Rate { get; set; }
-        //
-        // [BindProperty]
-        // public string Description { get; set; }
-        
-        [BindProperty]
-        public Movie Movie { get; set; }
-        
+        [BindProperty] public string Title { get; set; }
+
+        [BindProperty] public int Rate { get; set; }
+
+        [BindProperty] public string Description { get; set; }
+
+
+        private ApplicationDbContext _context;
+
+        public AddMovieModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
-
         }
 
         public IActionResult OnPost()
         {
-            string value = $"{Movie.Title}-{Movie.Rate}-{Movie.Description}";
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+
+            var movie = new Movie()
+            {
+                Title = Title,
+                Rate = Rate,
+                Description = Description
+            };
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            
             return Redirect("Movies");
         }
     }
